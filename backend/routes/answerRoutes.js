acd27127b0642902daa61faa0c0a30b0128dd776
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-08-11 17:42:07 +0300

    commit challenge sans db

---
+++ backend/routes/answerRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const { 
+    fetchAnswer, 
+    addAnswer, 
+    updateAnswer, 
+    deleteAnswer } = require('../controllers/answerController')
+
+router.route('/').get(fetchAnswer) 
+        
+router.route('/:id').post(addAnswer).put(updateAnswer).delete(deleteAnswer)            
+
+
+
+module.exports = router
---
+++ backend/routes/questionRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const { 
+    fetchQuestion, 
+    fetchQuestions, 
+    addQuestion, 
+    addQuestions, 
+    deleteQuestion, 
+    updateQuestion } = require('../controllers/questionController')
+
+router.route('/:id').get(fetchQuestion).post(addQuestion).put(updateQuestion).delete(deleteQuestion)
+
+
+router.route('/').get(fetchQuestions).post(addQuestions)
+      
+module.exports = router
