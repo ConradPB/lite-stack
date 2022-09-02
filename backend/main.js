31336a77a5ff90744f854cdd4953f1ccadecb6cf
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-09-02 12:28:43 +0300

    commit challenge update

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,7 @@
 const app = require('../backend/server')
+const port = process.env.PORT || 5000
+
 
 app.listen(port, () => console.log(`Its hot on port: ${port}`))
 
 
-module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const dotenv =  require('dotenv').config()
-const port = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
@@ -10,6 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/questions', require('./routes/questionRoutes'))
 
 app.use('/api/answers', require('./routes/answerRoutes'))
+
 
 
 module.exports = app
