2642d1c8f2558e2167830272795dcb17bd7e085e
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-09-05 06:08:20 +0300

    q&a controller and routes setup

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -1,53 +1,53 @@
+const asyncHandler = require('express-async-handler')
+
+
 //@desc     Get question
 //@route    GET /api/question
-
-const { text } = require("express")
-
 //@access   Private
-const fetchQuestion = (req,res) => {    
+const fetchQuestion = asyncHandler(async (req,res) => {    
     res.status(200).json({message: `Fetch question ${req.params.id}`})
-}
+})
 
 //@desc     Get questions
 //@route    GET /api/question
 //@access   Private
-const fetchQuestions = (req,res) => {
+const fetchQuestions = asyncHandler(async (req,res) => {
     res.status(200).json({message: 'Fetch all questions'})
-}
+})
 
 //@desc     Post question
 //@route    POST /api/question
 //@access   Private
-const addQuestion = (req,res) => {
+const addQuestion = asyncHandler(async (req,res) => {
     res.status(200).json({message: `Add question ${req.params.id}`})
-}
+})
 
 //@desc     Post all questions
 //@route    POST /api/question
 //@access   Private
-const addQuestions = (req,res) => {
+const addQuestions = asyncHandler(async (req,res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('please add some info in the field')
     }
 
     res.status(200).json({ message: 'Add all questions' })
-}
+})
 
 
 //@desc     Update question
 //@route    PUT /api/question
 //@access   Private
-const updateQuestion = (req,res) => {
+const updateQuestion = asyncHandler(async (req,res) => {
     res.status(200).json({message: `Update question ${req.params.id}`})
-}
+})
 
 //@desc     Delete question
 //@route    DELETE /api/question
 //@access   Private
-const deleteQuestion = (req,res) => {
+const deleteQuestion = asyncHandler(async (req,res) => {
     res.status(200).json({message: `Delete question ${req.params.id}`})
-}
+})
 
 module.exports = {
     fetchQuestion,
